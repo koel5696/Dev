2d0fb7a64a5c0a7f1287191de07283f79e69eb0e
koel5696 <128348671+koel5696@users.noreply.github.com>
2023-06-06 21:24:42 +0900

    방명록 과제

---
+++ 방명록_과제/GuestbookEntry.java
@@ -0,0 +1,69 @@
+package ch11; 
+ 
+import java.util.Date;
+
+public class GuestbookEntry {
+    private int id;
+    private java.sql.Date date;
+    private String title;
+    private String author;
+    private String email;
+    private String content;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public java.sql.Date getDate() {
+        return date;
+    }
+
+    public void setDate(java.sql.Date date) {
+        this.date = date;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
